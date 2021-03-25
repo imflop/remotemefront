1cb45c4e51599a7ab01063d62549ea3ee7812e0b
agafonovim <andrey@agafonov.im>
2021-03-25 03:31:18 +0300

    header initial

--- src/components/app/header/Header.tsx
+++ src/components/app/header/Header.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './Header.scss';
+import logotype from '../../../assets/img/logotype.svg';
 
 const Header = () => {
     return (
         <header className="header">
-            This is header
+            <div className="header__inner">
+                <NavLink to="/" className="logotype">
+                    <img src={logotype} className="logotype__img"/>
+                </NavLink>
+
+                <nav className="navbar">
+                    <div>
+asd
+                    </div>
+                    <NavLink to="/" className="post-button">
+                        Post a job
+                    </NavLink>
+                </nav>
+            </div>
         </header>
     );
 }
