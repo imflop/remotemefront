fc410f4097f67ed1b3a18b5303ddbf35b2de4193
agafonovim <andrey@agafonov.im>
2021-04-01 18:53:15 +0300

    job card initial config

--- src/components/job-card/JobCard.tsx
+++ src/components/job-card/JobCard.tsx
@@ -10,27 +10,22 @@ type Job = {
     scope: string;
     created_at: string,
     city: string,
+    company_name: string,
 };
 
-const JobCard: React.ElementType = ({uuid, short_description, created_at, city, scope}: Job) => {
+const JobCard: React.ElementType = ({uuid, short_description, created_at, company_name, city}: Job) => {
 
     const formattedDate = getHumanTimeFromNow(created_at);
 
     return (
-        <div className="card card_job">
-            <div className="card__body">
-                <Link to={`/jobs/${uuid}`}>
-                    {short_description}
-                </Link>
-            </div>
-            <div className="card__footer">
-                <ul className="list list_separator list_job-card">
-                    <li className="list_item">Posted {formattedDate}</li>
-                    <li className="list_item">{city}</li>
-                    <li className="list_item">{scope}</li>
-                </ul>
-            </div>
-        </div>
+        <Link to={`/jobs/${uuid}`} className="job-card">
+            <h2 className="job-card__title">{short_description}</h2>
+            <ul className="list list_separator list_job-details">
+                <li className="list__item">Posted {formattedDate}</li>
+                <li className="list__item">{company_name}</li>
+                <li className="list__item">{city}</li>
+            </ul>
+        </Link>
     );
 };
 
