dd50416680bf361bf8f3926c6a00f10695c41f90
imflop <imflop@gmail.com>
2021-05-07 21:36:25 +0300

    added icons to scope list

--- src/components/scope-filter/ScopeFilter.tsx
+++ src/components/scope-filter/ScopeFilter.tsx
@@ -5,12 +5,32 @@ import { Link } from 'react-router-dom';
 import './ScopeFilter.scss';
 
 const ScopeFilter: React.ElementType = ({id, title}: Scope) => {
+    type iconOptions = {
+        [key: string]: string
+    }
+
+    const icons: iconOptions = {
+        "Product Manager": "📧",
+        "Fullstack": "🛠",
+        "Blockchain": "🔗",
+        "Backend": "🖥",
+        "Frontend": "💻",
+        "DevOps": "🚀",
+        "Data Science": "🧬",
+        "QA": "🔎",
+        "iOS": "🍏",
+        "Mobile": "📱",
+        "Android": "☎️"
+    };
+
     return (
-        <li>
-            <Link to={`${id}`} className="scope-filter">
-                <span>🖥</span>
-                <h5 className="title">{title}</h5>
-            </Link>
+        <li className="text-center">
+            <div className="card">
+                <Link to={`${id}`} className="scope-filter">
+                    <span>{icons[title]}</span>
+                    <h5 className="title">{title}</h5>
+                </Link>
+            </div>
         </li>
     )
 }
